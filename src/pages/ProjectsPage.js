320fcc921cfc596243d2e7b0dd0dc87c38a61f49
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-12-14 13:52:10 +0300

    added ids to projects for linking

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/ProjectsPage.css";
+import { Link } from "react-router-dom";
 import ProjectCard from "../components/ProjectCard";
 import sampleImage1 from "../assets/sample-project1.jpeg";
 import sampleImage2 from "../assets/sample-project2.jpeg";
 
 const sampleProjects = [
   {
+    id: 1,
     image: sampleImage1,
     title: "Sunrise Residency",
     builtDate: "2023",
@@ -13,6 +15,7 @@ const sampleProjects = [
     isAvailable: true,
   },
   {
+    id: 2,
     image: sampleImage2,
     title: "Urban Heights",
     builtDate: "2021",
@@ -20,6 +23,7 @@ const sampleProjects = [
     isAvailable: false,
   },
   {
+    id: 3,
     image: sampleImage1,
     title: "Sunrise Residency",
     builtDate: "2023",
@@ -27,6 +31,7 @@ const sampleProjects = [
     isAvailable: true,
   },
   {
+    id: 4,
     image: sampleImage2,
     title: "Urban Heights",
     builtDate: "2021",
@@ -34,6 +39,7 @@ const sampleProjects = [
     isAvailable: false,
   },
   {
+    id: 5,
     image: sampleImage1,
     title: "Sunrise Residency",
     builtDate: "2023",
@@ -41,6 +47,7 @@ const sampleProjects = [
     isAvailable: true,
   },
   {
+    id: 6,
     image: sampleImage2,
     title: "Urban Heights",
     builtDate: "2021",
@@ -48,6 +55,7 @@ const sampleProjects = [
     isAvailable: false,
   },
   {
+    id: 7,
     image: sampleImage1,
     title: "Sunrise Residency",
     builtDate: "2023",
@@ -55,6 +63,7 @@ const sampleProjects = [
     isAvailable: true,
   },
   {
+    id: 8,
     image: sampleImage2,
     title: "Urban Heights",
     builtDate: "2021",
@@ -69,14 +78,16 @@ const ProjectsPage = () => {
       <h2>Our Projects</h2>
       <div className="project-list">
         {sampleProjects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            image={project.image}
-            title={project.title}
-            builtDate={project.builtDate}
-            totalApartments={project.totalApartments}
-            isAvailable={project.isAvailable}
-          />
+          <Link key={project.id} to={`/projects/${project.id}`}>
+            <ProjectCard
+              key={index}
+              image={project.image}
+              title={project.title}
+              builtDate={project.builtDate}
+              totalApartments={project.totalApartments}
+              isAvailable={project.isAvailable}
+            />
+          </Link>
         ))}
       </div>
     </div>
