b4832114c5e47118437c785869821b055d65b281
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-12-16 20:26:01 +0300

    admin navbar

---
+++ src/components/AdminNavbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath, useNavigate } from "react-router-dom";
+import "../styles/Navbar.css";
+
+export default function AdminNavbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/admin");
+  };
+
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Site Name
+      </Link>
+      <ul>
+        <CustomLink to="/admin/dashboard">Dashboard</CustomLink>
+        <CustomLink to="/admin/manage-projects">My Projects</CustomLink>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
