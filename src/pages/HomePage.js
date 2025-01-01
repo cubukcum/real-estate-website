0ddca122f6a8e561d31a185b21c3cb5285a6bc13
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2025-01-01 15:11:03 +0300

    imported project slider

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 import TopBanner from "../components/TopBanner";
 import FeaturesSection from "../components/FeaturesSection";
 import AchievementsSection from "../components/AchievementsSection";
+import ProjectsSlider from "../components/ProjectsSlider";
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <TopBanner />
+      <ProjectsSlider />
       <FeaturesSection />
       <AchievementsSection />
     </div>
